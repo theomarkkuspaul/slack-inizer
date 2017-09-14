90babaf088e5cba63096e37bbe8f664940f19411
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2017-09-14 21:59:05 +1200

    Add rest client gem

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -4,3 +4,10 @@ get '/users/:id' do
   @user = User.find(params[:id])
   erb :"users/show"
 end
+
+post '/randomize' do
+  payload = {first_name: "testo"}.to_json
+  binding.pry
+  resp = RestClient.post("https://slack.com/api/users.profile.set", {token: session[:access_token], profile: payload})
+
+end
--- config/environment.rb
+++ config/environment.rb
@@ -25,10 +25,8 @@ require 'erb'
 
 require 'rest-client'
 
-# load different enviroments
 require_relative 'development'
 
-# Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
